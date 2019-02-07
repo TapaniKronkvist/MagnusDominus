77b33411a344ef717f724674943a3e4b2c5a8b1e
TapaniKronkvist <45991745+TapaniKronkvist@users.noreply.github.com>
2019-02-07 14:52:33 +0100

    New Scenes and music

---
+++ UnityProject/Assets/Audio/AudioScripts/options.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class options : MonoBehaviour
+{
+    public AudioMixer masterMixer;
+    
+    // Start is called before the first frame update
+
+    public void SetMusicVol(float musicVol)
+    {
+        masterMixer.SetFloat("musicVol", musicVol);//set vol in a parameter
+    }
+    public void SetSfxVol(float sfxVol)
+    {
+        masterMixer.SetFloat("sfxVol", sfxVol);//set vol in a parameter
+    }
+}
---
+++ UnityProject/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Managing scene transitions.
+//Written by Tapani Kronkvist
+public class SceneManager : MonoBehaviour
+{
+    public void StartNewGame()
+    {
+        Application.LoadLevel("Overworld");
+    }
+    public void Options()
+    {
+        Application.LoadLevel("Options");
+    }
+    public void backToMain()
+    {
+        Application.LoadLevel("MainMenu");
+    }
+}
