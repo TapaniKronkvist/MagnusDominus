4ed280f8a9e534c415b5806d60b41dd946766aed
Juice-f <47383418+Juice-f@users.noreply.github.com>
2019-02-11 10:08:40 +0100

    camera

--- UnityProject/Assets/Scripts/EnemySpawner.cs
+++ UnityProject/Assets/Scripts/EnemySpawner.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [Range(0, 10)]
-    [SerializeField]
-    int minNumberOfEnemies;
-    [Range(0, 10)]
-    [SerializeField]
-    int maxNumberOfEnemies = 10;
-    [SerializeField]
+    //[Range(0, 10)]
+    //[SerializeField]
+    //int minNumberOfEnemies;
+    //[Range(0, 10)]
+    //[SerializeField]
+    //int maxNumberOfEnemies = 10;
+    //[SerializeField]
     float spawnRadius;
 
 
     [SerializeField]
-    Enemy[] testEnemies;
+    List<Enemy> testEnemies;
 
     private void Start()
     {
@@ -36,13 +36,13 @@ public class EnemySpawner : MonoBehaviour
         }
 #endif
     }
-    public void SpawnEnemies(Enemy[] enemiesToSpawnFrom)
+    public void SpawnEnemies(List<Enemy> enemiesToSpawn)
     {
-        int numberOfEnemiesToSpawn = Random.Range(minNumberOfEnemies, maxNumberOfEnemies);
-        Debug.Log("spawning " + numberOfEnemiesToSpawn + " enemies");
-        for (int i = 0; i < numberOfEnemiesToSpawn; i++)
+        //int numberOfEnemiesToSpawn = Random.Range(minNumberOfEnemies, maxNumberOfEnemies);
+        Debug.Log("spawning " + enemiesToSpawn.Count + " enemies");
+        for (int i = 0; i < enemiesToSpawn.Count; i++)
         {
-            GameObject newEnemy = Instantiate(enemiesToSpawnFrom[Random.Range(0, enemiesToSpawnFrom.Length)].gameObject, transform);
+            GameObject newEnemy = Instantiate(enemiesToSpawn[i].gameObject);
             newEnemy.transform.position = new Vector3(transform.position.x + Random.Range(-spawnRadius, spawnRadius), transform.position.y, transform.position.z + Random.Range(-spawnRadius, spawnRadius));
         }
     }
--- UnityProject/Assets/Scripts/WorldManager.cs
+++ UnityProject/Assets/Scripts/WorldManager.cs
@@ -9,9 +9,11 @@ public class WorldManager : MonoBehaviour
     // Start is called before the first frame update
     public enum Bosses { fire, nature, stone }
     List<Bosses> defeatedBosses = new List<Bosses>();
+    [SerializeField]
+    WorldState[] dungeonStates = new WorldState[3] { new WorldState("Fire Dungeon"), new WorldState("Nature Dungeon"), new WorldState("Stone Dungeon") };
 
     [SerializeField]
-    WorldState[] worldStates = new WorldState[4] { new WorldState("neutral"), new WorldState("fire"), new WorldState("nature"), new WorldState("stone") };
+    WorldState[] overWorldStates = new WorldState[4] { new WorldState("neutral"), new WorldState("fire"), new WorldState("nature"), new WorldState("stone") };
 
     [SerializeField]
     int overworldSceneNum, dungeonSceneNum;
@@ -36,17 +38,23 @@ public class WorldManager : MonoBehaviour
     {
         if (defeatedBosses.Count > 0)
         {
-            switch (defeatedBosses[defeatedBosses.Count-1])
+            switch (defeatedBosses[defeatedBosses.Count - 1])
             {
                 case Bosses.fire:
-                  //  ItemGenerator.ins.SpawnItems();
+                    ItemGenerator.ins.SpawnItems(overWorldStates[1].ItemDropTable);
                     break;
                 case Bosses.nature:
+                    ItemGenerator.ins.SpawnItems(overWorldStates[2].ItemDropTable);
                     break;
                 case Bosses.stone:
+                    ItemGenerator.ins.SpawnItems(overWorldStates[3].ItemDropTable);
                     break;
 
             }
+        }
+        else
+        {
+            ItemGenerator.ins.SpawnItems(overWorldStates[0].ItemDropTable);
         }
     }
 
@@ -71,15 +79,17 @@ public class WorldState
     public string name;
     [SerializeField]
     List<Pickup> itemDropTable = new List<Pickup>();
+    public List<Pickup> ItemDropTable { get => itemDropTable; }
+
     [SerializeField]
     List<GroupOfEnemies> enemyGroups = new List<GroupOfEnemies>();
-
+    public GroupOfEnemies[] EnemyGroups { get => enemyGroups.ToArray(); }
 }
 [System.Serializable]
 public class GroupOfEnemies
 {
 
-    [HideInInspector]
+    //    [HideInInspector]
     public string name;
     [SerializeField]
     List<Enemy> enemies = new List<Enemy>();
