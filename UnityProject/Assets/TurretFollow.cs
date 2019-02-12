390f46f9d63eb09d9f605d6369a13ba7f82ca890
Juice-f <47383418+Juice-f@users.noreply.github.com>
2019-02-12 10:41:26 +0100

    Enemy nose fixed

---
+++ UnityProject/Assets/TurretFollow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretFollow : MonoBehaviour
+{
+    [SerializeField]
+    GameObject objToFollow;
+    [SerializeField]
+    Vector3 offset;
+
+    [SerializeField]
+    bool smooth = false;
+    [SerializeField]
+    float moveSpeed = 10;
+
+    private void Start()
+    {
+        transform.position = objToFollow.transform.position + offset;
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (smooth)
+        {
+            transform.position =    Vector3.Lerp(transform.position,( objToFollow.transform.forward * objToFollow.transform.position.magnitude) + offset, moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = objToFollow.transform.forward * objToFollow.transform.position.magnitude + offset;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(objToFollow.transform.position + offset, .5f);
+    }
+}
