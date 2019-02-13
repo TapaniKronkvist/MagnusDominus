ec0472adf4b55651ed006e4ff327cd9628d716d6
Hippopotamusen <markus.broddd64@gmail.com>
2019-02-13 10:20:36 +0100

    DamageModifier

---
+++ UnityProject/Assets/Scripts/EquipmentScripts/DamageMod.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageMod : Equipment
+{
+    [SerializeField]
+    float modifier;
+
+
+    public override void ChangePlayerStats()
+    {
+
+        Playermanager.ins.damageModifier *= modifier;
+    }
+}
